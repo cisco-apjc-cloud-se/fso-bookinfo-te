2cea4d8c2e796e1fd8572b6f9c769a6dcf2a0c9a
Richard <richwats@cisco.com>
2021-11-04 11:33:39 +1100

    retry

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "thousandeyes_http_server" "http_tests" {
 
   content_regex = ".*"
 
-  network_measurements = 1
+  network_measurements = 0
   mtu_measurements = 0
   bandwidth_measurements = 0
   bgp_measurements = 0
